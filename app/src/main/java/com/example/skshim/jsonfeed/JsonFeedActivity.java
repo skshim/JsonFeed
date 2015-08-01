d45c9fa8ba233cf5237c17afc1a30abe89c2a35b
Sungki <skshim38@hotmail.com>
2015-08-01 14:34:09 +1000

    Add JSonFeedTask

--- app/src/main/java/com/example/skshim/jsonfeed/JsonFeedActivity.java
+++ app/src/main/java/com/example/skshim/jsonfeed/JsonFeedActivity.java
@@ -1,8 +1,11 @@
 package com.example.skshim.jsonfeed;
 
+import android.app.ProgressDialog;
 import android.content.Context;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
@@ -12,7 +15,21 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.gson.Gson;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -150,6 +167,119 @@ public class JsonFeedActivity extends AppCompatActivity {
             }
 
             return convertView;
+        }
+    }
+
+    /**
+     * JSonFeedTask : AsyncTask that retrieves JSon data from Web.
+     */
+    private class JSonFeedTask extends AsyncTask<String,Void,String> {
+
+        private ProgressDialog dialog = new ProgressDialog(JsonFeedActivity.this);
+        private boolean error=true;
+
+        @Override
+        protected void onPreExecute() {
+            dialog.setMessage("Downloading ...");
+            dialog.show();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+                error=false;
+                return downloadUrl(urls[0]);
+            } catch (IOException e) {
+                return "Unable to retrieve json data. URL may be invalid.";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            dialog.dismiss();
+
+            if(error){
+                Toast.makeText(JsonFeedActivity.this, result, Toast.LENGTH_LONG)
+                        .show();
+            }else{
+                // Convert string to mFactList and provide it to FactAdapter.
+                displayFeed(result);
+            }
+        }
+
+        @Override
+        protected void onCancelled(String s) {
+            dialog.dismiss();
+        }
+    }
+
+    // Given a URL, establishes an HttpUrlConnection and retrieves
+    // the json content as a InputStream, which it returns as
+    // a string.
+    private String downloadUrl(String myurl) throws IOException {
+        InputStream inputStream = null;
+        try {
+            URL url = new URL(myurl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            conn.setReadTimeout(10000 /* milliseconds */);
+            conn.setConnectTimeout(15000 /* milliseconds */);
+            conn.setRequestMethod("GET");
+            conn.setDoInput(true);
+
+            // Starts the query
+            conn.connect();
+            int response = conn.getResponseCode();
+
+            inputStream = conn.getInputStream();
+
+            // Convert the InputStream into a string
+            String contentAsString = readIt(inputStream);
+            return contentAsString;
+
+            // Makes sure that the InputStream is closed after the app is
+            // finished using it.
+        } finally {
+            if (inputStream != null) {
+                inputStream.close();
+            }
+        }
+    }
+    // Reads an InputStream and converts it to a String.
+    public String readIt(InputStream stream) throws IOException, UnsupportedEncodingException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream, "UTF-8"));
+        StringBuilder out = new StringBuilder();
+        String line;
+        while((line=reader.readLine()) != null){
+            out.append(line);
+        }
+        return out.toString();
+    }
+
+    // Convert string to mFactList and provide it to FactAdapter.
+    private void displayFeed(String result){
+        JSONObject responseObj=null;
+        try{
+            Gson gson=new Gson();
+            responseObj=new JSONObject(result);
+            String mainTitle=responseObj.getString("title");
+            JSONArray rowItems=responseObj.getJSONArray("rows");
+
+            mFactList =new ArrayList<Fact>();
+            Fact rowItem;
+            for(int i=0; i<rowItems.length(); i++){
+                rowItem = gson.fromJson(rowItems.getJSONObject(i).toString(),Fact.class);
+
+                if(!rowItem.isNull()){
+                    mFactList.add(rowItem);
+                }
+            }
+
+            mFactAdapter.setItemList(mFactList);
+
+        }catch (JSONException e){
+            e.printStackTrace();
         }
     }
 
