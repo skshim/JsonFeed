7ec2cc52731cff47cc4475a12d5bceefc7ee85ed
Sungki <skshim38@hotmail.com>
2015-08-01 12:35:39 +1000

    Add FactAdapter that backs ListView

--- app/src/main/java/com/example/skshim/jsonfeed/JsonFeedActivity.java
+++ app/src/main/java/com/example/skshim/jsonfeed/JsonFeedActivity.java
@@ -1,11 +1,19 @@
 package com.example.skshim.jsonfeed;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -42,8 +50,101 @@ public class JsonFeedActivity extends AppCompatActivity {
     }
 
     /**
-     *
+     * FactAdapter : The main adapter that backs the ListView.
      */
+    private class FactAdapter extends ArrayAdapter<Fact> {
+
+        //
+        private class ViewHolder{
+            TextView title;
+            TextView description;
+            ImageView imageView;
+        }
+
+        private List<Fact> mItemList;
+
+        public FactAdapter(Context context, int resource, ArrayList<Fact> facts){
+            super(context,resource,facts);
+            mItemList=facts;
+        }
+
+        /**
+         * {@inheritDoc}
+         */
+        @Override
+        public int getCount() {
+            if(mItemList==null){
+                return 0;
+            }else{
+                return mItemList.size();
+            }
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * @param position
+         */
+        @Override
+        public Fact getItem(int position) {
+            return mItemList.get(position);
+        }
+
+        /**
+         * {@inheritDoc}
+         *
+         * @param position
+         */
+        @Override
+        public long getItemId(int position) {
+            if(mItemList==null){
+                return 0;
+            }else{
+                return getItem(position).hashCode();
+            }
+        }
+
+        /**
+         *  JSon data should be converted into List<Fact> and passed to function
+         *
+         * @param itemList
+         */
+        public void setItemList(List<Fact> itemList) {
+            mItemList = itemList;
+            this.notifyDataSetChanged();
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            ViewHolder holder;
+            if(convertView ==null){
+                LayoutInflater layInf=(LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                convertView = layInf.inflate(R.layout.item_fact,null);
+
+                holder = new ViewHolder();
+                holder.title=(TextView)convertView.findViewById(R.id.title);
+                holder.description=(TextView)convertView.findViewById(R.id.description);
+                holder.imageView =(ImageView)convertView.findViewById(R.id.imageView);
+
+                convertView.setTag(holder);
+
+            }else{
+                holder =(ViewHolder) convertView.getTag();
+            }
+
+            Fact fact = getItem(position);
+            holder.title.setText((fact.getTitle() != null ? fact.getTitle() : ""));
+            holder.description.setText((fact.getDescription()!=null?fact.getDescription():""));
+            if(fact.getImageHref()==null){
+                holder.imageView.setVisibility(View.GONE);
+            }else{
+                // load the imageView asynchronously into the ImageView
+                holder.imageView.setVisibility(View.VISIBLE);
+            }
+
+            return convertView;
+        }
+    }
 
 
 }
