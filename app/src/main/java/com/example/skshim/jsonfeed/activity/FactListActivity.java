fa1bd5cc641937672ac6e4750ce997712089aca7
Sungki <skshim38@hotmail.com>
2015-08-02 21:13:19 +1000

    Check internet connectivity before refresh.

--- app/src/main/java/com/example/skshim/jsonfeed/activity/FactListActivity.java
+++ app/src/main/java/com/example/skshim/jsonfeed/activity/FactListActivity.java
@@ -1,5 +1,8 @@
 package com.example.skshim.jsonfeed.activity;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -7,6 +10,7 @@ import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.skshim.jsonfeed.R;
 import com.example.skshim.jsonfeed.adapter.FactAdapter;
@@ -18,7 +22,7 @@ import com.example.skshim.jsonfeed.model.FeedResult;
 import java.util.ArrayList;
 
 
-public class FactListActivity extends AppCompatActivity implements  SwipeRefreshLayout.OnRefreshListener{
+public class FactListActivity extends AppCompatActivity {
 
     private SwipeRefreshLayout mSwiptLayout;
     private ArrayList<Fact> mFactList;
@@ -38,7 +42,12 @@ public class FactListActivity extends AppCompatActivity implements  SwipeRefresh
 
         // Config swip layout
         mSwiptLayout = (SwipeRefreshLayout) findViewById(R.id.swipe_refresh_layout);
-        mSwiptLayout.setOnRefreshListener(this);
+        mSwiptLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener(){
+            @Override
+            public void onRefresh() {
+                refresh();
+            }
+        });
         mSwiptLayout.setColorSchemeResources(android.R.color.holo_blue_bright,
                 android.R.color.holo_green_light,
                 android.R.color.holo_orange_light,
@@ -67,23 +76,35 @@ public class FactListActivity extends AppCompatActivity implements  SwipeRefresh
         }
     }
 
-    private void refresh(){
-        if(!mSwiptLayout.isRefreshing()){
-            mSwiptLayout.setRefreshing(true);
-        }
-        mActionBar.setTitle(R.string.loading);
-        new FactAsyncTask(this, new FactAsyncTask.OnFeedResultListener() {
-            @Override
-            public void onFeedResult(FeedResult feedResult) {
-                displayFeed(feedResult);
+    // Refresh only when network is connected.
+    private void refresh() {
+        if (isConnected()) {
+            if (!mSwiptLayout.isRefreshing()) {
+                mSwiptLayout.setRefreshing(true);
+            }
+            mActionBar.setTitle(R.string.loading);
+            new FactAsyncTask(this, new FactAsyncTask.OnFeedResultListener() {
+                @Override
+                public void onFeedResult(FeedResult feedResult) {
+                    displayFeed(feedResult);
+                    mSwiptLayout.setRefreshing(false);
+                }
+            }).execute(Constants.FEED_URL);
+        }else{
+            if (mSwiptLayout.isRefreshing()) {
                 mSwiptLayout.setRefreshing(false);
             }
-        }).execute(Constants.FEED_URL);
+            mActionBar.setTitle(R.string.no_internet_title);
+            Toast.makeText(this,R.string.no_internet,Toast.LENGTH_LONG)
+                    .show();
+        }
     }
 
-    @Override
-    public void onRefresh() {
-        refresh();
+    // Check if network is available.
+    private boolean isConnected(){
+        ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        return activeNetwork != null && activeNetwork.isConnectedOrConnecting();
     }
 
     /**
